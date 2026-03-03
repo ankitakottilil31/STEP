228945e6b940df918036efa5e8a6e23a88cc07fe
ankitakottilil31 <ankita.kottilil31@gmail.com>
2026-03-03 10:43:48 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,39 +1,65 @@
-package UC_4;
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        String[] O = {
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
-        };
-        String[] P = {
-                " ******",
-                "*     *",
-                "*     *",
-                " ******",
-                "*      ",
-                "*      ",
-                "*      "
-        };
-        String[] S = {
-                " ******",
-                "*      ",
-                "*      ",
-                " ***** ",
-                "      *",
-                "      *",
-                " ******"
-        };
-        String[] lines = new String[7];
+
+        String[] o1 = getOPattern();
+        String[] o2 = getOPattern();  // Reuse method (DRY Principle)
+        String[] p  = getPPattern();
+        String[] s  = getSPattern();
+
+        String[] banner = new String[7];
+
         for (int i = 0; i < 7; i++) {
-            lines[i] = String.join("   ", O[i], O[i], P[i], S[i]);
+            banner[i] = String.join(" ",
+                    o1[i],
+                    o2[i],
+                    p[i],
+                    s[i]
+            );
         }
-        for (String line : lines) {
+
+        // Enhanced for loop to print
+        for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Helper method for O
+    public static String[] getOPattern() {
+        return new String[] {
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        };
+    }
+
+    // Helper method for P
+    public static String[] getPPattern() {
+        return new String[] {
+                "  ****** ",
+                " *     * ",
+                "*       *",
+                " ******  ",
+                "*        ",
+                "*        ",
+                "*        "
+        };
+    }
+
+    // Helper method for S
+    public static String[] getSPattern() {
+        return new String[] {
+                "  ****** ",
+                " *       ",
+                "*        ",
+                "  *****  ",
+                "       * ",
+                " *     * ",
+                "  *****  "
+        };
     }
 }
