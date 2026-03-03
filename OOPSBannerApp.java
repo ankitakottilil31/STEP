3c19899d0963f552da173a3786a7fb36125edd6a
ankitakottilil31 <ankita.kottilil31@gmail.com>
2026-03-03 10:43:05 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,37 +1,50 @@
-package UC_4;
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        String[] O = {
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
+        String[] lines = {
+
+                String.join("   ",
+                        " ***** ",
+                        " ***** ",
+                        " ******",
+                        " ******"),
+
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*     *",
+                        "*      "),
+
+                String.join("   ",
+                        "*     *",
+                        "*     *",
+                        "*     *",
+                        "*      "),
+
+                String.join("   ",
+                        "*     *",
+                        " ***** ",
+                        " ******",
+                        " ***** "),
+
+                String.join("   ",
+                        "*     *",
+                        "*      ",
+                        "*      ",
+                        "      *"),
+
+                String.join("   ",
+                        "*     *",
+                        "*      ",
+                        "*      ",
+                        "      *"),
+
+                String.join("   ",
+                        " ***** ",
+                        "*      ",
+                        "*      ",
+                        " ******")
         };
-        String[] P = {
-                " ******",
-                "*     *",
-                "*     *",
-                " ******",
-                "*      ",
-                "*      ",
-                "*      "
-        };
-        String[] S = {
-                " ******",
-                "*      ",
-                "*      ",
-                " ***** ",
-                "      *",
-                "      *",
-                " ******"
-        };
-        String[] lines = new String[7];
-        for (int i = 0; i < 7; i++) {
-            lines[i] = String.join("   ", O[i], O[i], P[i], S[i]);
-        }
         for (String line : lines) {
             System.out.println(line);
         }
