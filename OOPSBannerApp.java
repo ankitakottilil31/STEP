fb81398fc88d26025e723574926112f79dd19fdf
ankitakottilil31 <ankita.kottilil31@gmail.com>
2026-03-03 10:25:54 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,39 @@
+package UC_4;
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        String[] O = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+        String[] P = {
+                " ******",
+                "*     *",
+                "*     *",
+                " ******",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+        String[] S = {
+                " ******",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ******"
+        };
+        String[] lines = new String[7];
+        for (int i = 0; i < 7; i++) {
+            lines[i] = String.join("   ", O[i], O[i], P[i], S[i]);
+        }
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
