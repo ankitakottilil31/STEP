4e7d0cb3e627fa32806c9517a422314d70469eab
ankitakottilil31 <ankita.kottilil31@gmail.com>
2026-03-10 11:07:58 +0530

    Add files via upload

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,39 +1,61 @@
-package UC_4;
+import java.util.HashMap;
+
 public class OOPSBannerApp {
+
+    public static HashMap<Character, String[]> createCharacterMap() {
+
+        HashMap<Character, String[]> charMap = new HashMap<>();
+
+        charMap.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        charMap.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        charMap.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+
+        return charMap;
+    }
+
+    public static void displayBanner(String message, HashMap<Character, String[]> charMap) {
+
+        int patternHeight = charMap.get('O').length;
+
+        for (int line = 0; line < patternHeight; line++) {
+
+            StringBuilder row = new StringBuilder();
+
+            for (char c : message.toCharArray()) {
+                String[] pattern = charMap.get(c);
+                row.append(pattern[line]).append("  ");
+            }
+
+            System.out.println(row.toString());
+        }
+    }
+
     public static void main(String[] args) {
-        String[] O = {
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
-        };
-        String[] P = {
-                " ******",
-                "*     *",
-                "*     *",
-                " ******",
-                "*      ",
-                "*      ",
-                "*      "
-        };
-        String[] S = {
-                " ******",
-                "*      ",
-                "*      ",
-                " ***** ",
-                "      *",
-                "      *",
-                " ******"
-        };
-        String[] lines = new String[7];
-        for (int i = 0; i < 7; i++) {
-            lines[i] = String.join("   ", O[i], O[i], P[i], S[i]);
-        }
-        for (String line : lines) {
-            System.out.println(line);
-        }
+
+        HashMap<Character, String[]> charMap = createCharacterMap();
+
+        String message = "OOPS";
+
+        displayBanner(message, charMap);
     }
 }
